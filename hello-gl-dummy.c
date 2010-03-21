2e9ad1725313773df32ec6fc55346d1d23848efd
Joe Groff <arcata@gmail.com>
2010-03-21 01:22:12 -0700

    add a "hello-gl-dummy" version that just contains the glut setup code

---
+++ hello-gl-dummy.c
@@ -0,0 +1,52 @@
+#include <GL/glew.h>
+#ifdef __APPLE__
+#  include <GLUT/glut.h>
+#else
+#  include <GL/glut.h>
+#endif
+#include <stdio.h>
+
+static int make_resources(void)
+{
+}
+
+/*
+ * GLUT callbacks:
+ */
+static void update_fade_factor(void)
+{
+}
+
+static void render(void)
+{
+    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+/*
+ * Entry point
+ */
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(400, 300);
+    glutCreateWindow("Hello World");
+    glutIdleFunc(&update_fade_factor);
+    glutDisplayFunc(&render);
+
+    glewInit();
+    if (!GLEW_VERSION_2_0) {
+        fprintf(stderr, "OpenGL 2.0 not available\n");
+        return 1;
+    }
+
+    if (!make_resources()) {
+        fprintf(stderr, "Failed to load resources\n");
+        return 1;
+    }
+
+    glutMainLoop();
+    return 0;
+}
+
